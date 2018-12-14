bc4ad6c0d59448ac725208e75fc128046c12b3e6
Adam Singh <adam@drillmap.com>
2018-12-14 08:13:43 -0500

    Add files via upload
    More clean up, commenting and making the code more efficient.

--- Code-from-the-Past/PHP/as400calls.php
+++ Code-from-the-Past/PHP/as400calls.php
@@ -3,6 +3,7 @@
 // FILE CREATED BY BY ASINGH - 11/28/2018
 // EDITED FOR AD LOGIN BY ASINGH - 12/07/2018
 // CLEANED UP AND EDITED FOR MINI ORANGE AUTHENTICATION BY ASINGH - 12/10/2018
+// CLEANED UP AND EDITED SO IT RETRIEVES CREDENTIALS ON USER CHANGE BUT NOT ON EVERY PAGE LOAD - 12/12/2018
 
 /* There are three lines of code that need to be put into the haines-portal-100-width.php file
  *
@@ -23,7 +24,7 @@
  *
  * exit();
  *
- * should be placed right before get_footer();
+ * should be placed right after get_footer();
  */
 
 //Path for Web Service Calls, Data Table Names and Login Redirect
@@ -33,10 +34,10 @@ define('HC_USERS', "");
 define('HC_REDIRECT', "Location:  ");
 
 //Credential Functions//////////////////////////////////////////////////////////////////////////////////////////////////////
-// These functions need the user's credentials to operate
+// These functions use or change the user's credentials in the session.
 
 //WP User Retrieval
-//Function Accesses the user through cookies
+//Function accesses the username from the cookies
 function wp_user_retrieval()
 {
     //For Loop for Cookies
@@ -47,12 +48,11 @@ function wp_user_retrieval()
         {
             $matches = array();
             preg_match("/^[^\|]*/", $value, $matches);
-            return $matches[0];
-        }
+            return trim($matches[0]);
+        }//End of if(preg_match("/^wordpress_logged_in/", $key))
     }//End of foreach($_COOKIE as $key => $value)
 
-    header(HC_REDIRECT); /* Redirect browser */
-    die("User is not logged into wordpress.");
+   death("User is not logged into wordpress.");
 }//End of function wp_user_retrieval()
 
 //Database Connect function
@@ -61,10 +61,10 @@ function open_db()
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); // Create MySQL connection
 
     // Check connection for error
-    if($conn->connect_error)
+    if($message = $conn->connect_error)
     {
-        header(HC_REDIRECT); /* Redirect browser */
-        //die("Connection failed: " . $conn->connect_error); //Debugging Test
+        close_db($conn);
+        death("Connection failed: ".$message);
     }
 
     return $conn; //Return the connecton
@@ -83,12 +83,11 @@ function run_query($conn, $query)
     //If the query fails, close the connection and redirect to the user to the login page
     if(!$result = $conn->query($query))
     {
-        close_db($conn); //Closing Database
-        header(HC_REDIRECT); /* Redirect browser */
-        //die("Could not query"); // Debugging Test
-    }
+        close_db($conn);
+        death("Could not query.");
+    }//End of if(!$result = $conn->query($query))
 
-    return $result;
+    return $result; //return successful query
 }//End of function run_query($conn, $query)
 
 //Extract Session ID's from the JSON
@@ -103,64 +102,89 @@ function extractSessionID($json)
             if(preg_match("/sesid/", $key))
                 return $value;
 
-                //If the value is an array, make a recursive call.
-                // I shouldn't really need this else if but hey, just to be safe.
+            //If the value is an array, make a recursive call.
+            // I shouldn't really need this else if but hey, just to be safe.
             else if(is_array($value))
                 return extractSessionID($value);
 
         }//End of foreach($json as $key => $value)
 
-        return "";  //Return an empty string if there is nosession key in the array.
+        return "";  //Return an empty string if there is no session key in the array.
     }//End of if(is_array($json))
 
-    return null;  //returns null if there was an array was not present when needed.
+    return null;  //returns null if an array was not pasted in as a paramater.
 }//End of function extractSessionID($json)
 
 //Login function
 function portal_login()
 {
-    //$username = wp_user_retrieval(); //Retrieving wordpress user name
-    $username = "portalae";
+    //Retrieving wordpress user name and checking to see if it is in the session
+    /*$username = wp_user_retrieval();
+    if(!isset($_SESSION['username']) || $_SESSION['username'] != $username)
+    {
+        $_SESSION['username'] = $username;
+        $new_user = true;
+    }
+
+    else
+    {
+        $new_user = false;
+    }*/
+
+    if(!isset($_SESSION['username'])) $_SESSION['username'] = "portalae"; //Temporary until user table is setup
     //echo $username."<br>"; //Debugging statement
 
-    $conn = open_db(); // Create connection
-    $query = "SELECT * FROM `".HC_USERS."` WHERE `Username` = \"".$username."\""; //Create query to retrieve Dancik Creendtials
-    $result = run_query($conn, $query);
-
-    //Retrieve results of sucessful Dancik Credentials query and store all retrieved credentials in the session
-    $dancik = $result->fetch_assoc();
-    $_SESSION['dancik_user'] = $dancik['DancikUsername'];
-    $_SESSION['dancik_pass'] = $dancik['DancikPassword'];
-    $_SESSION['role'] = $dancik['RoleID'];
-    $_SESSION['username'] = $username;
-
-    //Making Login Web Service Call to Dancik and decoding the JSON
-    $json_str = file_get_contents(HC_PATH."/login/?d24user=".$_SESSION['dancik_user']."&d24pwd=".$_SESSION['dancik_pass']);
-    $hold = json_decode($json_str, true);
-
-    if(isset($hold['errors'])) // If JSON returned is an error message conditional
+    //Checking to see if Dancik Username, Dancik Password and Role are stored in the session or if there is a new user
+    //If not or new user, go get'em
+    if(/*$new_user*/!isset($_SESSION['dancik_user']) ||!isset($_SESSION['dancik_pass']) || !isset($_SESSION['role']))
     {
-        close_db($conn); //Closing Database
-        //Location will need to be changed when code is pushed.
-        header(HC_REDIRECT); /* Redirect browser */
-        exit();
-        //die("Fails to log into Dancik."); // Debugging Test
-    }//End of if(isset($hold['errors']))
+        $conn = open_db(); // Create connection
+        $query = "SELECT `DancikUsername`, `DancikPassword`, `RoleID` FROM `".HC_USERS."` WHERE UPPER(`Username`) = \"".strtoupper($_SESSION['username'])."\""; //Create query to retrieve Dancik Creendtials
+        $result = run_query($conn, $query);
 
-    else //No error is returned conditional
-    {
-        //Storing Dancik Session ID and removing password.
-        $_SESSION['sesid'] = extractSessionID($hold);
-        unset($_SESSION['dancik_pass']);
-
-        //Dancik Call to get account ID and store in the session
-        $json_str = file_get_contents(HC_PATH."/getAccountInfo?d24user=".$_SESSION['dancik_user']."&d24sesid=".$_SESSION['sesid']);
-        $hold = json_decode($json_str, true);
-        $accountInfo = $hold['acct'];
-        $_SESSION['acctid'] = $accountInfo['accountid'];
-
+        //Retrieve results of sucessful Dancik Credentials query and store all retrieved credentials in the session
+        $dancik = $result->fetch_assoc();
+        if(!isset($_SESSION['dancik_user'])) $_SESSION['dancik_user'] = $dancik['DancikUsername'];
+        if(!isset($_SESSION['dancik_pass'])) $_SESSION['dancik_pass'] = $dancik['DancikPassword'];
+        if(!isset($_SESSION['role'])) $_SESSION['role'] = $dancik['RoleID'];
         close_db($conn); //Closing Database connection
-    }//End of no error is returned conditional
+    }//End of if($new_user !isset($_SESSION['dancik_user']) ||!isset($_SESSION['dancik_pass']) || !isset($_SESSION['role']))
+
+    //Checking to see if Dancik Seesion ID and Account ID are stored in the session or if there is a new user
+    //If not or new user, go get'em
+    if(/*$new_user*/!isset($_SESSION['sesid']) || !isset($_SESSION['acctid']))
+    {
+        //Making Login Web Service Call to Dancik and decoding the JSON
+        $json_str = file_get_contents(HC_PATH."/login/?d24user=".$_SESSION['dancik_user']."&d24pwd=".$_SESSION['dancik_pass']);
+        $hold = json_decode($json_str, true);
+
+        if(isset($hold['errors'])) // If JSON returned is an error message conditional
+        {
+            death("Fails to log into Dancik.");
+        }//End of if(isset($hold['errors']))
+
+        else //No error is returned conditional
+        {
+            //Conditional for storing Dancik Session ID and removing password
+            if(/*$new_user*/!isset($_SESSION['sesid']))
+            {
+                $_SESSION['sesid'] = extractSessionID($hold);
+                unset($_SESSION['dancik_pass']);
+            }//End of  if($new_user !isset($_SESSION['sesid']))
+
+            //Conditional for Dancik Call to get account ID and store in the session
+            if(/*$new_user*/!isset($_SESSION['acctid']))
+            {
+                $json_str = file_get_contents(HC_PATH."/getAccountInfo?d24user=".$_SESSION['dancik_user']."&d24sesid=".$_SESSION['sesid']);
+                $hold = json_decode($json_str, true);
+                $accountInfo = $hold['acct'];
+                $_SESSION['acctid'] = $accountInfo['accountid'];
+            }//End of if($new_user !isset($_SESSION['acctid']))
+        }//End of no error is returned conditional
+    }//End of if($new_user !isset($_SESSION['sesid']) ||!isset($_SESSION['acctid']))
+
+    //Unset the dancik password in the session, just in case of something weird
+    if(isset($_SESSION['dancik_pass'])) unset($_SESSION['dancik_pass']);
 }//End of function portal_login()
 
  //Logout Function
@@ -178,28 +202,44 @@ function portal_logout()
      $_SESSION = array();
      session_destroy();
 }//End of function portal_logout()
+
+//Get Credential String
+//Returns a string for entering credentials for Dancik RESTful Calls
+function get_credentials()
+{
+    return "?d24user=".$_SESSION['username']."&d24sesid=".$_SESSION['sesid']."&d24_acctid=".$_SESSION['acctid'];
+}//End of function get_credentials()
+
+//Death Function
+//Kills page and session if something goes wrong and redirects user to the home page
+function death($message)
+{
+    portal_logout(); //Clearing Session
+    header(HC_REDIRECT); /* Redirect browser */
+    die($message);
+}//End of function death($message)
 //End of Credential Functions///////////////////////////////////////////////////////////////////////////////////////////////////
 
-//SEARCHING FUNCTIONS///////////////////////////////////////////////////////////////////////////////////////////////////////
+//SEARCHING FUNCTIONS///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // These functions make web service calls to Dancik or send queries to the MySQL database.
 // Then the search function calls the appropriate print function
 //Product Search Function
 function productSearch()
 {
-    $json_str = file_get_contents(HC_PATH."getItemsForAccount?d24user=".$_SESSION['username']."&d24sesid=".$_SESSION['sesid']."&d24_acctid=".$_SESSION['acctid']);
+    $json_str = file_get_contents(HC_PATH."getItemsForAccount".get_credentials());
     $products = json_decode($json_str, true);
-    $items = $products['items'];
-    printItems($items);
+    if(isset($products['errors'])) death("Service call returned an error."); //If the service call is bad, call death.
+    printItems($products['items']);
 }//End of productSearch()
 
 //Price Search Function
 // It used to go here http://jjh400:8000/price-catalogs
 function priceSearch()
 {
-    $json_str = file_get_contents(HC_PATH."getPriceList?d24user=".$_SESSION['username']."&d24sesid=".$_SESSION['sesid']."&d24_acctid=".$_SESSION['acctid']);
+    $json_str = file_get_contents(HC_PATH."getPriceList".get_credentials());
     $prices = json_decode($json_str, true);
-    $best_prices = $prices['best_price'];
-    printBestPrices($best_prices);
+    if(isset($prices['errors'])) death("Service call returned an error."); //If the service call is bad, call death.
+    printBestPrices($prices['best_price']);
 }//End of productSearch()
 
 // Mill Claim Search
@@ -263,12 +303,13 @@ function mill_claim_search()
              }*/
 
             $query .= " AND `".$key."` = \"".$value."\"";
+            //$query .= " AND `".$key."` LIKE \"%".$value."%\"";
             //$first = false;
         }
     }
 
-    //Retrieving query results
-    printMillClaim(run_query($conn, $query)); //Printing query results table
+    //Retrieving and printing query results table
+    printMillClaim(run_query($conn, $query));
 
     // Conditional to check query and if the user entered search data
     /*if((isset($conditions['Claim Number']) || isset($conditions['Claim Status']) ||
@@ -323,7 +364,7 @@ function print_invoices($invoices)
     {
         $table .= "<tr>"; //Start new row
         //For Loop for each individual invoice
-        foreach($invoice as $key => $value)
+        foreach($invoice as $value)
         {
             //Print the table row
             $table .= "<td align=\"left\">".$value."</td>";
